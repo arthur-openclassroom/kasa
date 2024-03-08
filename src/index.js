abc34327d7c1f778c9f1988095915d9dbc61fa74
Arthur Lambotte <arthur1209@orange.fr>
2024-03-08 11:47:42 +0100

    init project, route, sass

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './sass/main.scss'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Estate from './pages/Estate/Estate';
+import Notfound from './pages/Notfound/Notfound';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/estate/:id" element={<Estate />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
---
+++ src/pages/Estate/Estate.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Estate = () => {
+  return (
+    <div>
+      Estate
+    </div>
+    );
+};
+
+export default Estate;
