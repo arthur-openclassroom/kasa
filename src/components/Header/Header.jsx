a6e258ab81422c8f0b7e835ebb5740fe161b83b1
Arthur Lambotte <arthur1209@orange.fr>
2024-03-08 15:10:09 +0100

    Header finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/svg/logo.svg"
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-    );
+    <header className="header-component">
+      <img className="logo" src={logo} alt="logo" />
+      <nav className="menu">
+        <NavLink to="/"
+          className={({ isActive }) =>
+            isActive ? "active" : ""
+          }>
+          Accueil
+        </NavLink>
+        <NavLink to="/about"
+          className={({ isActive }) =>
+            isActive ? "active" : ""
+          }>
+          À propos
+        </NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
