239f64c3c60dcbfac304b4fda454c19559c5fc6e
Arthur Lambotte <arthur1209@orange.fr>
2024-03-15 12:34:53 +0100

    Dropdown component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Tag from "../../components/Tag/Tag";
 import Card from "../../components/Card/Card";
+import Dropdown from "../../components/Dropdown/Dropdown";
 const Home = () => {
   return (
     <div>
       Home
-      <Tag label={"TestTesttettestestesttestst"}/>
+      <Tag label={"Tesesttettestestesttestst"}/>
       <Card titre={"TestTesttettestestesttestst"} img={"https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"} />
+      <Dropdown title={"Équipements"} labelList={["Climatisation", "Wi-Fi", "Cuisine", "Espace de travail", "Fer à repasser"]} />
     </div>
     );
 };
