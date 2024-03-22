db84746eaf42ecbf136df1469bf72ae69382c1b6
Arthur Lambotte <arthur1209@orange.fr>
2024-03-22 17:00:33 +0100

    about pages / notfound pages

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,11 +1,39 @@
 import React from "react";
+import Dropdown from "../../components/Dropdown/Dropdown";
+import Banner from "../../components/Banner/Banner";
+import AboutBannerPicture from "../../assets/img/about.jpeg";
 
 const About = () => {
+  const aboutList = [
+    {
+      title: 'Fiabilité',
+      content:
+        'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
+    },
+    {
+      title: 'Respect',
+      content:
+        'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+    },
+    {
+      title: 'Service',
+      content:
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      title: 'Sécurité',
+      content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
   return (
-    <div>
-      
+    <div className="about-page">
+      <Banner banner={AboutBannerPicture} />
+      <div className="dropdowns-container">
+        {aboutList.map((aboutElement) => <Dropdown key={aboutElement.title} title={aboutElement.title} labelList={[aboutElement.content]} />)}
+      </div>
     </div>
-    );
+  );
 };
 
 export default About;
